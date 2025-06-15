c12b509e1a772d721c0d77de2ec1cb189835b502
Muhammad Mujtaba Hussain <mmujtabah1599@gmail.com>
2025-06-15 10:29:09 +0500

    Added Proper favicons for different devices

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import "./globals.css";
+<meta name="apple-mobile-web-app-title" content="NovahelmStudios" />
 
 export const metadata: Metadata = {
     title: "Novahelm Studios",
